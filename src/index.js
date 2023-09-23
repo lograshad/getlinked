6c17813d32c3daa4a2e3d32908ade60a2dd21b62
Rashad <akintola.rd@gmail.com>
2023-09-23 08:25:16 +0100

    clean up in Progress

--- src/index.js
+++ src/index.js
@@ -21,29 +21,20 @@ root.render(
 reportWebVitals();
 
 // todos
-// smooth scroll
 // gsap
 // framer motion
 // image masking
 // animate stars yoyo
 // svgs responsiveness
-// cursor animation
 // add music
 // press a key anywhere to contact
-// delete, cleanup unwanted files, like fonts images and stuff
-// button interactivity
-// stop highlight on text
 // add ellipse to that babes head and remove the second one
-// web title and picture
 // carefully crosscheck one by one
 // comment code and uncomment
 // bounce man on cloud
 // position stars, glowball and flares to responsiveness 
 // page load on image download
-// navbar links interactivity
-// fix brand name issue with gsap
 // fix text area
 // fix boy filter
-// write the todos left in a notebook
 // add read me
 // fix text area text color
