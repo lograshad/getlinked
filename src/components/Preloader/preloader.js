a2cf468202e8a8bb34d8d686e01c77628ad76a2a
Rashad <akintola.rd@gmail.com>
2023-09-23 23:56:31 +0100

    last minute fix

--- src/components/Preloader/preloader.js
+++ src/components/Preloader/preloader.js
@@ -1,50 +1,42 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from '../../Screens/HomeScreen/home';
-import "./preloader.css";
+import './preloader.css';
 
-class Preloader extends Component {
-    constructor() {
-        super();
-        this.state = {
-            isLoading: true,
-        };
-    }
+function Preloader( {updateCurse, updateLeave}) {
+  const [isLoading, setIsLoading] = useState(true);
 
-    componentDidMount() {
-        // Add an event listener to the 'load' event of the window
-        window.addEventListener('load', () => {
-            // Hide the preloader when the window is fully loaded
-            this.setState({ isLoading: false });
-        });
-    }
-    componentWillUnmount() {
-        // Remove the event listener when the component is unmounted
-        window.removeEventListener('load', () => {});
-    }
+  useEffect(() => {
+    // Add an event listener to the 'load' event of the window
+    window.addEventListener('load', () => {
+      // Hide the preloader when the window is fully loaded
+      setIsLoading(false);
+    });
 
-    render() {
-        const { isLoading, updateCurse, updateLeave } = this.state;
+    // Remove the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('load', () => {});
+    };
+  }, []);
 
-        return isLoading ? (
-            <div className="preloader">
-                <div class="pyramid-loader">
-                    <div class="wrapper">
-                        <span class="side side1"></span>
-                        <span class="side side2"></span>
-                        <span class="side side3"></span>
-                        <span class="side side4"></span>
-                        <span class="shadow"></span>
-                    </div>
-                </div>
-                <p>The Awesomeness awaits, give it a minute</p>
-            </div>
-        ) : (
-            <Home
-                updateCurse={updateCurse}
-                updateLeave={updateLeave}
-            />
-        );
-    }
+  return isLoading ? (
+    <div className="preloader">
+      <div className="pyramid-loader">
+        <div className="wrapper">
+          <span className="side side1"></span>
+          <span className="side side2"></span>
+          <span className="side side3"></span>
+          <span className="side side4"></span>
+          <span className="shadow"></span>
+        </div>
+      </div>
+      <p>The Awesomeness awaits, give it a minute</p>
+    </div>
+  ) : (
+    <Home 
+        updateCurse={updateCurse}
+        updateLeave={updateLeave}
+    />
+  );
 }
 
 export default Preloader;
